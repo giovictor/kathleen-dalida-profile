5477e8e2425fe5cd535975308f0379dca099b6ce
gio <giorodriguez021@gmail.com>
2019-06-29 15:46:27 +0800

    Changed font sizes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,12 @@
 import React from "react"
+import Menu from '../menu'
 
 import styles from '../../styles/contact.module.css'
 
 const Contact = () => (
     <div className={styles.contact} id="contact">
         <div className={styles.menu}>
-            
+            <Menu mode="contactNav" />
         </div>
 
         <div className={styles.details}>
