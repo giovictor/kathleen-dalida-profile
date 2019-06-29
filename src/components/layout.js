05bce2fc1da71489a4e4c2ab41b0e48f52e2a65e
gio <giorodriguez021@gmail.com>
2019-06-29 18:57:01 +0800

    Added favicom

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ const Layout = ({children}) => (
     <div className="profile-container">
         <Helmet>
             <title>Kath Dalida</title>
-            <link rel="icon" href={favicon} type="image/png" sizes="16x16"/>
+            <link rel="shortcut icon" type="image/png" href={favicon} />
             <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap" rel="stylesheet"/>      
         </Helmet>
         {children}
